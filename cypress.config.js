d751fb47ec13fde29c10d77761e9948c9fcb0b09
Sara Peca <sara.sistemas@gmail.com>
2025-03-26 20:49:22 -0300

    adicionando .gitignore e adicionando report mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportTitle: 'Projeto cypress iniciante',
+      reportPageTitle: 'Projeto cypress iniciante',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
     e2e: {
       baseUrl : "https://automationpratice.com.br/",
       defaultCommandTimeout: 5000,
       setupNodeEvents(on, config) {
-      // implement node event listeners here
+        require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
